bbd31e7475bde15a4521804522b5d15fa640ebf1
yjy116 <yjy116@gmail.com>
2026-05-04 06:12:55 +0800

    Sync VIKINGYFY CI template updates

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2026 VIKINGYFY
 
 PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
@@ -21,54 +23,34 @@ if [ -d *"homeproxy"* ]; then
 
 	cd .. && rm -rf ./$HP_RULE/
 
-	# HomeProxy relies on its uci-defaults script to register fw4 nftables includes.
-	# On sysupgrade with preserved settings, package uci-defaults scripts may not rerun,
-	# which leaves HomeProxy installed but without the firewall include sections.
-	HP_DEFAULTS="./base-files/files/etc/uci-defaults/99_homeproxy_fw4_${WRT_DATE:-custom}"
-	mkdir -p "$(dirname "$HP_DEFAULTS")"
-	cat > "$HP_DEFAULTS" <<'EOF'
-#!/bin/sh
-
-[ -f "/www/luci-static/resources/icons/loading.gif" ] && \
-	sed -i "s,/loading.svg,/loading.gif,g" "/www/luci-static/resources/view/homeproxy/status.js"
-
-uci -q batch <<EOT >"/dev/null"
-delete firewall.homeproxy_forward
-set firewall.homeproxy_forward=include
-set firewall.homeproxy_forward.type='nftables'
-set firewall.homeproxy_forward.path='/var/run/homeproxy/fw4_forward.nft'
-set firewall.homeproxy_forward.position='chain-pre'
-set firewall.homeproxy_forward.chain='forward'
-delete firewall.homeproxy_input
-set firewall.homeproxy_input=include
-set firewall.homeproxy_input.type='nftables'
-set firewall.homeproxy_input.path='/var/run/homeproxy/fw4_input.nft'
-set firewall.homeproxy_input.position='chain-pre'
-set firewall.homeproxy_input.chain='input'
-delete firewall.homeproxy_post
-set firewall.homeproxy_post=include
-set firewall.homeproxy_post.type='nftables'
-set firewall.homeproxy_post.path='/var/run/homeproxy/fw4_post.nft'
-set firewall.homeproxy_post.position='table-post'
-commit firewall
-EOT
-
-exit 0
-EOF
-	chmod 0755 "$HP_DEFAULTS"
-
 	cd $PKG_PATH && echo "homeproxy date has been updated!"
 fi
 
 #修改argon主题字体和颜色
 if [ -d *"luci-theme-argon"* ]; then
-	echo " "
-
-	cd ./luci-theme-argon/
+	echo " " && cd ./luci-theme-argon/
 
 	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
 
 	cd $PKG_PATH && echo "theme-argon has been fixed!"
+fi
+
+#修改aurora菜单式样
+if [ -d *"luci-app-aurora-config"* ]; then
+	echo " " && cd ./luci-app-aurora-config/
+
+	sed -i "s/nav_submenu_type '.*'/nav_submenu_type 'boxed-dropdown'/g" $(find ./root/usr/share/aurora/ -type f -name "*.template")
+
+	cd $PKG_PATH && echo "theme-aurora has been fixed!"
+fi
+
+#修改mini-diskmanager菜单位置
+if [ -d *"luci-app-mini-diskmanager"* ]; then
+	echo " " && cd ./luci-app-mini-diskmanager/
+
+	sed -i "s/services/system/g" ./luci-app-mini-diskmanager/root/usr/share/luci/menu.d/luci-app-mini-diskmanager.json
+
+	cd $PKG_PATH && echo "mini-diskmanager has been fixed!"
 fi
 
 #修改qca-nss-drv启动顺序
@@ -109,25 +91,4 @@ if [ -f "$RUST_FILE" ]; then
 	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
 
 	cd $PKG_PATH && echo "rust has been fixed!"
-fi
-
-#修复DiskMan编译失败
-DM_FILE="./luci-app-diskman/applications/luci-app-diskman/Makefile"
-if [ -f "$DM_FILE" ]; then
-	echo " "
-
-	sed -i -E 's/fs-ntfs([^[:alnum:]_-]|$)/fs-ntfs3\1/g' "$DM_FILE"
-	sed -i '/ntfs-3g-utils /d' "$DM_FILE"
-
-	cd $PKG_PATH && echo "diskman has been fixed!"
-fi
-
-#移除sb内核回溯移植补丁
-SB_PATCH="../feeds/packages/net/sing-box/patches"
-if [ -d "$SB_PATCH" ]; then
-	echo " "
-
-	rm -rf $SB_PATCH
-
-	cd $PKG_PATH && echo "sing-box patches has been fixed!"
 fi
